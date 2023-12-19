5f7328fffedb459c9a054f9d10bb659f65d8a92a
Omri Bar-Haim <obarhaim@redhat.com>
2023-12-19 11:09:47 +0200

    update aro delete script

--- delete_aro_cluster.sh
+++ delete_aro_cluster.sh
@@ -9,8 +9,9 @@ export CLUSTER=$CLUSTER_NAME
 export RESOURCEGROUP=$ARO_RG
 export ARO_VNET="aro-vnet"
 
+# Delete the cluster
+az aro delete --resource-group $RESOURCEGROUP --name $CLUSTER
+
 # Delete virtual network created for the cluster subnets
 az network vnet delete --name $ARO_VNET --resource-group $RESOURCEGROUP
 
-# Delete the cluster
-az aro delete --resource-group $RESOURCEGROUP --name $CLUSTER
